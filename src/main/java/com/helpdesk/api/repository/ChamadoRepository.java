a4922980edc5adfed5f1b865589a02697a3a0c2f
davidfrott <silvadavz70@gmail.com>
2026-09-10 14:11:44 -0300

    Atualiza README do projeto

--- src/main/java/com/helpdesk/api/repository/ChamadoRepository.java
+++ src/main/java/com/helpdesk/api/repository/ChamadoRepository.java
@@ -5,8 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChamadoRepository extends JpaRepository<Chamado, Long> {
-    
-    // Busca Inteligente: O Spring entende o nome do método e gera
-    // o SQL para buscar chamados filtrando pelo ID do usuário automaticamente!
     List<Chamado> findByUsuarioId(Long usuarioId);
 }
